459d1151cda8590f5e8c12e5178b44c16d0c9b2e
klod <klod9288@gmail.com>
2017-11-12 10:35:14 +0700

    Pager and Tablayout success

---
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/SupplierRegisterFragment.java
@@ -0,0 +1,25 @@
+package srongklod_bangtamruat.plantseconomic.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import srongklod_bangtamruat.plantseconomic.R;
+
+/**
+ * Created by Administrator on 12/11/2560.
+ */
+
+public class SupplierRegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_supplier_register, container, false);
+        return view;
+    }
+}//Main Class
