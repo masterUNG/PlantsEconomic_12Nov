6d3e26257d9c83d11367bf4073924a743ab59c52
klod <klod9288@gmail.com>
2017-11-12 09:50:32 +0700

    Tablayout OK

--- app/src/main/java/srongklod_bangtamruat/plantseconomic/MainActivity.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/MainActivity.java
@@ -21,4 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     }//onCreate
 
+    @Override
+    public void onBackPressed() {
+
+    }//cannotCall back
 }//Main Class
--- app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/RegisterFragment.java
+++ app/src/main/java/srongklod_bangtamruat/plantseconomic/fragment/RegisterFragment.java
@@ -2,11 +2,14 @@ package srongklod_bangtamruat.plantseconomic.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import srongklod_bangtamruat.plantseconomic.MainActivity;
 import srongklod_bangtamruat.plantseconomic.R;
 
 /**
@@ -14,6 +17,52 @@ import srongklod_bangtamruat.plantseconomic.R;
  */
 
 public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Create Toolbar
+        createToolbar();
+
+//        Create Tablayout
+        createTablayout();
+
+
+    }//Main Method
+
+    private void createTablayout() {
+
+        TabLayout tabLayout = getView().findViewById(R.id.tablayoutRegister);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.Customer));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.supplier));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.Transport));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_CENTER);//Char is Center
+    }
+
+    private void createToolbar() {
+//        Setup Toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolbarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+//        SetTitle Toolbar
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getResources()
+        .getString(R.string.register));
+
+//        set Navigator icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);//Click wapwap
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             getActivity().getSupportFragmentManager().popBackStack();//back page frit
+
+
+            }
+        });
+
+
+    }
 
     @Nullable
     @Override
